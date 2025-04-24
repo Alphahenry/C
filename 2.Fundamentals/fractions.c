8b4f4e742be9291947ebd1c2142ce92e67303222
Alphahenry <alphahenry2019@gmail.com>
2025-04-24 23:21:06 +0300

    first commit

---
+++ 2.Fundamentals/fractions.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int num1,denom1, num2, denom2;
+
+    printf("Enter num1 and denom1: ");
+    scanf("%d/%d", &num1,&denom1);
+
+    printf("Enter num2 and denom2: ");
+    scanf("%d/%d", &num2, &denom2);
+
+    int res_num = num1 * denom2 + num2 * denom1;
+
+    int res_den = denom1 * denom2;
+
+    printf("The sum is: %d/%d\n", res_num, res_den);
+    return 0;
+}

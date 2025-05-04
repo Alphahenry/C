5890b7b042c317512c7ff4d43ce6c79b6c0b9757
Alphahenry <alphahenry2019@gmail.com>
2025-05-04 07:15:49 +0300

    added if stement

--- 5.selections/time.c
+++ 5.selections/time.c
@@ -6,7 +6,7 @@ int main(){
     //declare format for 24hr
     int f1_24, f2_24;
 
-    //format for 12hr
+    //midnight
 
     int f1_12 = 12;
     int f2_12 = 00;
@@ -15,6 +15,25 @@ int main(){
 
     printf("Enter 24hr time: ");
     scanf("%2d:%2d", &f1_24, &f2_24);
+    
+    int D1_12hr =  f1_24-f1_12;
+    int D2_12hr = f2_24- f2_12;
+
+
+    if (f1_24 >12 && f1_24 < 24 )
+    {
+          printf("the time in 12 hr format is: %d:%dpm\n", D1_12hr,D2_12hr);
+
+    }else if (f1_24 <= 12)
+    {
+        printf("the time in 12 hr format is: %d:%dam\n", D1_12hr,D2_12hr);
+
+    }else{
+        printf("Wrong format!");
+    }
+    
+    
+    
 
 
     printf("the time in 12 hr format is: %d:%d\n", f1_24-f1_12, f2_24- f1_12);

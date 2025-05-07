20756aa098efb8a62273e7acfc046ba8461d7e21
Alphahenry <alphahenry2019@gmail.com>
2025-05-07 21:27:23 +0300

    first commit

---
+++ 6.Loops/Example1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int i = 1;
+    int n = 10;
+    while (i < n)/*Controlling expression*/
+    {
+        i = i + 2;/*loop body*/
+    }
+    
+
+    return 0;
+}

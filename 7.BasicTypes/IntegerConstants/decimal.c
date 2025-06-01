96499846a7c6625300857a4b42bcaedaf219d919
Alphahenry <alphahenry2019@gmail.com>
2025-06-01 17:17:20 +0300

    decimal

---
+++ 7.BasicTypes/IntegerConstants/decimal.c
@@ -0,0 +1,16 @@
+//decimal constants in C
+//decimalconstants conatins digit between 0 and 9. and must not begin with zero
+#include<stdio.h>
+
+
+int main(){
+
+    int x = 10; 
+    int x2 = 011;
+
+
+    printf("%d\n", x);
+    printf("%d\n", x2);
+
+    return 0;
+}

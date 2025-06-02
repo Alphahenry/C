666d0f21ea8dc52d315e756db6239abf1d7c7d3f
Alphahenry <alphahenry2019@gmail.com>
2025-06-02 20:50:27 +0300

    first commit

---
+++ 7.BasicTypes/IntegerConstants/long.c
@@ -0,0 +1,13 @@
+//long integer
+#include<stdio.h>
+int main(){
+    long int_x;
+
+    printf("Enter integer: ");
+    scanf("%ld", &int_x);
+
+    printf("%ld", int_x);
+
+
+    return 0;
+}
---
+++ 7.BasicTypes/IntegerConstants/short.c
@@ -0,0 +1,15 @@
+///short integers
+#include<stdio.h>
+
+int main(){
+
+    short p;
+
+    printf("enter your age: ");
+    scanf("%hd", &p);
+
+
+
+    printf("%d\n", p);
+    return 0;
+}

7bbebfe1efc4ee3730f7d3a1d0878df0ef5c0510
Alphahenry <alphahenry2019@gmail.com>
2025-06-21 23:06:30 +0300

    first

---
+++ 7.BasicTypes/projects/phone.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+   char  A,B,C = '2';
+   char  D,E,F = '3';
+   char  G,H,I = '4';
+   char  J,K,L = '5';
+   char  M,N,O = '6';
+   char  P,Q,R,S = '7';
+   char  T,U,V = '8';
+   char  W,X,Y,Z = '9';
+
+
+   printf("%c%c%c%c%c%c%c",C,L,L,A,T,X,X);
+
+
+
+
+
+    return 0;
+}

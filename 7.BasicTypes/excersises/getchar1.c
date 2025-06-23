b774856ec07911f0a54e12bdbb5014979d4237a6
Alphahenry <alphahenry2019@gmail.com>
2025-06-23 20:55:02 +0300

    first commit

---
+++ 7.BasicTypes/excersises/getchar1.c
@@ -0,0 +1,15 @@
+
+//how a buffer occurs
+#include<stdio.h>
+
+int main(){
+
+    char ch;
+
+    printf("enter a character; ");
+    ch = getchar();
+    printf("You entered: %c\n", ch);
+
+
+    return 0;
+}

6392a96815526372f93412b8f8afaa71bec535b4
Alphahenry <alphahenry2019@gmail.com>
2025-07-02 23:16:42 +0300

    first commit

---
+++ 8.Arrays/intro/ex2.c
@@ -0,0 +1,18 @@
+//initialize array to all zeroes
+#include<stdio.h>
+#define N 10
+
+
+int main(){
+    printf("Enter no of elements in array: ");
+   int z[N] = {0};
+
+  for(int i = 0; i < N; i++){
+     
+   printf(" %d", z[i]);
+  }
+
+
+
+    return 0;
+}

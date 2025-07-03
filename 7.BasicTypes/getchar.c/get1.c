0b789e3cac8f7a870c7e8459472b9769f1a70eb5
Alphahenry <alphahenry2019@gmail.com>
2025-07-03 19:04:27 +0300

    first commit

---
+++ 7.BasicTypes/getchar.c/get1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+   //using getchar()
+   char ch;
+
+   printf("Enter character: ");
+   ch = getchar();
+
+  printf("entered is: %c\n", ch);
+
+   //printf("You entered: %c\n", ch);
+
+
+    return 0;
+}

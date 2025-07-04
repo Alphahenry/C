99963dded2b86c6a033b6c8f01f8864ccdd4670f
Alphahenry <alphahenry2019@gmail.com>
2025-07-04 20:48:32 +0300

    sum

--- 7.BasicTypes/IntegerConstants/sum.c
+++ 7.BasicTypes/IntegerConstants/sum.c
@@ -1,23 +1,25 @@
 //summing a series of numbers
 #include<stdio.h>
+#include<limits.h>
 
 int main(){
-    long n, sum = 0;
+    long double n, sum = 0;
     
     printf("This program sums a series of integers\n");
     printf("Enter integers (enter 0 to terminate): \n");
 
-    scanf("%ld", &n);
+    scanf("%lf", &n);
 
     //loop to sum integers
 
+
     while (n != 0){
         sum += n;
-        scanf("%ld",&n);
+        scanf("%lf",&n);
 
     }
 
-    printf("The sum is %ld\n", sum);
+    printf("The sum is %lf\n", sum);
 
     
 

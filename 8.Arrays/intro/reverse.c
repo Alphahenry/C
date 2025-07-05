645f91971a31aeb9255123cdc5fff441874d9c86
Alphahenry <alphahenry2019@gmail.com>
2025-07-05 17:58:23 +0300

    reverse

--- 8.Arrays/intro/reverse.c
+++ 8.Arrays/intro/reverse.c
@@ -1,14 +1,28 @@
-/*program to reverse an array*/
-
 #include<stdio.h>
 
-//define a constant 10
+//define a constant
+
 #define N 10
 
 int main(){
-     int arr[10];
-     int
 
+    int a[10];
+    int i;
+
+    //read in an array
+    printf("Enter %d numbers: ", N);
+    for(i = 0; i < N; i++){
+        
+
+        scanf("%d", &a[i]);
+    }
+
+    //reverse the array
+    for ( i = N -1; i >= 0; i--)
+    {
+        printf(" %d", a[i]);
+    }
+    
 
     return 0;
 }

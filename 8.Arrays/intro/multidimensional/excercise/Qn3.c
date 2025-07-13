5e26517ca4940234770efa6db19e94ef3047c288
Alphahenry <alphahenry2019@gmail.com>
2025-07-13 19:12:13 +0300

    first commit

---
+++ 8.Arrays/intro/multidimensional/excercise/Qn3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    
+    //create an array of bool named weekend
+
+    int weekend[7] = {true, false, false,false,false,false, true };
+    return 0;
+}

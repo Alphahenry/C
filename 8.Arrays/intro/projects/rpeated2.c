19cbf35f2bbae08ed31786b41ac91113488abe13
Alphahenry <alphahenry2019@gmail.com>
2025-08-01 20:24:03 +0300

    first commit

---
+++ 8.Arrays/intro/projects/rpeated2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+
+    //declare variable to store number entered
+    long num;
+
+    //declare array to show number of digits and initialize as 0
+
+    int occurences[10] = {false};
+    int  digit;
+
+   //read the num
+
+   printf("Enter the number: ");
+   scanf("%ld", &num);
+
+
+   while(num > 0){
+    digit  = num % 10;
+    occurences[digit]++;
+    num  = num / 10;
+
+   }
+
+   printf("Digits: 0 1 2 3 4 5 6 7 8 9\n");
+   printf("Ocurrences:");
+
+   for (int i = 0; i < 10; i++)
+   {
+    printf("%d",occurences[i]);
+   }
+   
+   
+return 0;
+
+}

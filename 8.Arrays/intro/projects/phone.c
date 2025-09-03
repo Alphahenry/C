acee8581cc46d2289b817052d781ca58a26d715a
Alphahenry <alphahenry2019@gmail.com>
2025-09-03 04:55:15 +0300

    fixed num

--- 8.Arrays/intro/projects/phone.c
+++ 8.Arrays/intro/projects/phone.c
@@ -34,36 +34,36 @@ int main(){
     switch (Phone_No[i])
     {
                 case 'A': case 'B': case 'C':
-                    putchar('2');
+                    Phone_No[i] = putchar('2');
                     break;
 
 
                 case 'D': case 'E': case 'F':
-                putchar('3');
+                Phone_No[i] = putchar('3');
                 break;
 
                 case 'G': case 'H': case 'I':
-                putchar('4');
+                Phone_No[i] = putchar('4');
                 break;
 
                 case 'J': case 'K': case 'L':
-                putchar('5');
+                Phone_No[i]= putchar('5');
                 break;
 
                 case 'M': case 'N': case 'O':
-                putchar('6');
+                Phone_No[i] = putchar('6');
                 break;
 
                 case 'P': case 'R': case 'S':
-                putchar('7');
+                Phone_No[i] = putchar('7');
                 break;
 
                 case 'T': case 'U': case 'V':
-                    putchar('8');
+                 Phone_No[i]  = putchar('8');
                     break;
 
                 case 'W': case 'X': case 'Y':
-                    putchar('9');
+                 Phone_No[i] =   putchar('9');
                     break;
                 
                 
@@ -76,6 +76,9 @@ int main(){
     }
 
  }
+
+ //in numeric form 
+ printf("In numeric form: %s", Phone_No);
  
   
     

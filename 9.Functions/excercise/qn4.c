8d2ce847f39ae171ba13eed599e7bce812cbabef
Alphahenry <alphahenry2019@gmail.com>
2025-09-18 20:52:43 +0300

    first commit

---
+++ 9.Functions/excercise/qn4.c
@@ -0,0 +1,17 @@
+//function trhat returns day onth year
+
+#include<stdio.h>
+
+void day_of_year(month, day, year);
+int main(){
+   //get user input
+
+   int month, day,year;
+
+   printf("Enter month, day, year: %d,%d,%d: \n");
+   scanf("%d,%d,%d", &month,&day,&year);
+   
+
+    return 0;
+}
+

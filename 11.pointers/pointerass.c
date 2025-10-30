c7ae33f8448715d52ebd9da494aa2bdccafb5846
Alphahenry <alphahenry2019@gmail.com>
2025-10-31 00:37:08 +0300

    firstcommit

---
+++ 11.pointers/pointerass.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+
+
+    int i,j, *p, *q;
+    i = 1;
+
+    //copy address of i into p
+//example one of assigning pointer var
+    p = &i;
+
+    //example 2
+
+    q = p;
+
+    //address of i(cintents of p) is copied to q
+
+   *q =2;
+
+   printf("The value of i is : %d\n", i);
+
+
+
+
+    return 0;
+}

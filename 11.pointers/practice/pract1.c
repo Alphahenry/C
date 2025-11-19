1e07773a533c8b7dd2890d9a0b5331aa02829f37
Alphahenry <alphahenry2019@gmail.com>
2025-11-19 11:45:02 +0300

    address of p

--- 11.pointers/practice/pract1.c
+++ 11.pointers/practice/pract1.c
@@ -10,5 +10,9 @@ int main(){
   printf("The value of i is also: %d\n", i);
 
 
+//address of thge pointer
+printf("address is : %p\n", &i);
+printf("another way is: %p\n", (void*)&i);
+
     return 0;
 }

571be1955d4eb46a5ceab9a3381f37e50ce1ecc3
Alphahenry <alphahenry2019@gmail.com>
2026-01-19 23:33:43 +0300

    first escape

---
+++ 12.Strings/intro/escape.c
@@ -0,0 +1,9 @@
+//Escape sequence
+#include<stdio.h>
+
+int main(){
+
+    printf(" This is \n how you \n use an \n escape sequence\n");
+
+    return 0;
+}
---
+++ 12.Strings/intro/ex1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+
+char name[] = {"welcome to derry"};
+
+char s1[] = "lovely lady";
+
+    return 0;
+}

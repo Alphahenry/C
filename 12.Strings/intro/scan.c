46f60e9c774b20a5cec5c746e47b713d1be5dada
Alphahenry <alphahenry2019@gmail.com>
2026-01-26 22:55:34 +0300

    first commit

---
+++ 12.Strings/intro/scan.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+   char name[20];
+
+   printf("Enter name: \n");
+
+
+   //using scanf
+   scanf("%s", name);
+
+  printf("%s", name);
+
+
+
+    return 0;
+
+}

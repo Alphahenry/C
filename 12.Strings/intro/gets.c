1f9105fb554c048d4bc6c8b8877599dd6f0993f5
Alphahenry <alphahenry2019@gmail.com>
2026-01-31 05:11:51 +0300

    first commit

---
+++ 12.Strings/intro/gets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//using gets
+
+int main(){
+    
+    char name[30];
+
+
+    printf("Enter your name: ");
+
+    puts(name);
+
+    printf("%s", name);
+
+
+    return 0;
+}
---
+++ 12.Strings/intro/length.c
@@ -0,0 +1,14 @@
+//length of a string
+#include<stdio.h>
+#include<string.h>
+
+
+int main(){
+   char name[30] = "Alpha";
+   
+
+   printf( "The length is %ld",strlen(name));
+
+
+    return 0;
+}

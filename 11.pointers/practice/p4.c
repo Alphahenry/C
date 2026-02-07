e7a09da19f3f1a4271663e4e4a0beb9e1f6e0060
Alphahenry <alphahenry2019@gmail.com>
2026-02-07 22:46:12 +0300

    triple pointer

--- 11.pointers/practice/p4.c
+++ 11.pointers/practice/p4.c
@@ -7,6 +7,7 @@ int main(){
    int a, *z;
 
    int **m;
+   
 
    a = 10;
    z = &a;
@@ -16,7 +17,12 @@ int main(){
    printf("the value of a is: %d\n", **m);
    printf("another way of getting  value of a is: %d\n", *z);
 
+ int ***s = &m;
+   printf("Value of a through pointer s is = %d", ***s);
 
+   //changing value of a through pointer s
+   ***s = 25;
+   printf("New value of a is = %d ", **m);
 
     return 0;
 }

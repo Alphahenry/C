0ce0c6915aac90c0cf421aa3c9ca56b30290b718
Alphahenry <alphahenry2019@gmail.com>
2026-03-16 22:55:10 +0300

    escape

--- 12.Strings/intro/escape.c
+++ 12.Strings/intro/escape.c
@@ -1,9 +1,10 @@
-//Escape sequence
+//Escape sequence using \n
 #include<stdio.h>
 
 int main(){
 
     printf(" This is \n how you \n use an \n escape sequence\n");
+    printf(" This is \n how to use  \n another escape sequence\n");
 
     return 0;
 }

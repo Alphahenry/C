162f1384d42e1b605ac862ac594d92aab5c3c1c0
Alphahenry <alphahenry2019@gmail.com>
2026-03-27 19:57:06 +0300

    definite

--- 9.Functions/Practice/declaringarray.c
+++ 9.Functions/Practice/declaringarray.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 
-//creating an array with definiete size
+//creating an array with definite size
 
-int myarr(int n, int arry[n])
+int myarr(int n, int arry[n]);
 
 int main(){
 

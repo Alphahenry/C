9b24262836ef9752cb546c8a742cedfd7e20550c
Alphahenry <alphahenry2019@gmail.com>
2026-04-01 22:33:14 +0300

    first commit

--- 9.Functions/lessons/lesson2b.c
+++ 9.Functions/lessons/lesson2b.c
@@ -3,16 +3,27 @@
 #include<stdio.h>
 
 void sum(int a, int b);
+void sum2(int x, int y);
 
 
 int main(){
+    sum2(10,10);
     sum(4,3);
+
     return 0;
 }
 
+void sum2(int x, int y){
+
+    printf("Enter value of a and b: \n");
+    scanf("%d,%d", &x,&y);
+    int summ2 = x + y;
+    printf("The sum is %d\n",summ2);
+
+};
 
 void sum(int a, int b){
 
-    int sum = a + b;
-    printf("The sum is %d\n", sum);
+    int summ = a + b;
+    printf("The sum is %d\n", summ);
 }

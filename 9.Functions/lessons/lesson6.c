717272370573b5e6d23ec12666706bb34e238535
Alphahenry <alphahenry2019@gmail.com>
2026-04-12 09:38:23 +0300

    changed function void

--- 9.Functions/lessons/lesson6.c
+++ 9.Functions/lessons/lesson6.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 
-float sum(float a, float b);
+void sum(float a, float b);
 int main(){
     float a, b;
     printf("Enter number a and b: ");
@@ -13,7 +13,7 @@ int main(){
     return 0;
 }
 
-float sum(float a, float b){
+void sum(float a, float b){
     float sum = a + b;
 
     printf("The sum is: %f\n", sum);

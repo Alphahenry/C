c3877c04ed029ae7fdcad8230d4579f4fe3e138f
Alphahenry <alphahenry2019@gmail.com>
2026-04-14 21:19:25 +0300

    first commit

---
+++ 12.Strings/lessons/StringsandPointers.c
@@ -0,0 +1,12 @@
+//to use pinters as strings
+
+#include <stdio.h>
+
+#define STR_len 80
+
+int main(){
+   char str[STR_len + 1], *p;
+
+   p = str;
+    return 0;
+}
---
+++ 12.Strings/lessons/stringvariables.c
@@ -0,0 +1,12 @@
+#include  <stdio.h>
+
+#define STR_LEN 80
+//when defining string length consider adding length for the null character
+
+int main(){
+
+//when declaring add null character
+   char Str[STR_LEN + 1];
+
+    return 0;
+}

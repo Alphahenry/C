f7631e37aea47f97848686476efcbab0e9091f15
Alphahenry <alphahenry2019@gmail.com>
2026-07-21 21:10:17 +0300

    first commit

---
+++ 13.Structures/lesson1.c
@@ -0,0 +1,23 @@
+//student details
+
+
+#include<stdio.h>
+
+//declaring.(template no memory is allocated in the struct)
+struct student
+{
+    /* data */
+    int adm_no;
+    int form;
+    float marks;
+    char name[20];
+
+
+};
+
+
+
+int main(){
+   struct student s3;
+    return 0;
+}
